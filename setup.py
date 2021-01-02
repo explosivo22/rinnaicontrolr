180ab115b095d848f126deb4fc3cb12843496325
explosivo22 <barbourbj@gmail.com>
2021-01-02 08:52:05 -0600

    Add files via upload

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="rinnaicontrol-r",
-    version="0.0.1",
+    name="rinnaicontrolr",
+    version="0.0.9",
     description="Python interface for Rinnai Control-R API",
-    url="https://github.com/explosivo22/rinnaicontrol-r",
+    url="https://github.com/explosivo22/rinnaicontrolr",
     author="Brad Barbour",
     author_email="barbourbj@gmail.com",
     license='Apache Software License',
     install_requires=[ 'requests>=2.0' ],
     keywords=[ 'rinnai', 'home automation', 'water heater' ],
-    packages=[ 'rinnaicontrol-r' ],
+    packages=[ 'rinnaicontrolr' ],
     zip_safe=True,
     classifiers=[
         "Programming Language :: Python :: 3",
