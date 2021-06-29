325892cb172568108eec99dfec95bd154ae4e994
Brad Barbour <barbourbj@gmail.com>
2021-06-28 20:16:06 -0500

    add a private async request function to handle all the requests and adjust other functions to use it

--- rinnaicontrolr/base.py
+++ rinnaicontrolr/base.py
@@ -1,5 +1,6 @@
 import secrets, hashlib, hmac, base64
-import datetime, json, logging, re, six, time
+import json, logging, re, six
+from datetime import datetime, timedelta, time
 from typing import Optional
 
 import requests
@@ -77,23 +78,58 @@ def calculate_u(big_a, big_b):
 class RinnaiWaterHeater(object):
     # Represents a Rinnai Water Heater, with methods for status and issuing commands
 
-    def __init__(self, username, password):
-        self._username = username
-        self._password = password
-        self._pool_id = POOL_ID
-        self._client_id = CLIENT_ID
-        self._big_n = hex_to_long(N_HEX)
-        self._g = hex_to_long(G_HEX)
-        self._k = hex_to_long(hex_hash('00' + N_HEX + '0' + G_HEX))
-        self._small_a_value = self.generate_random_small_a()
-        self._large_a_value = self.calculate_a()
-        self._access_token = None
-        self._expires_in = None
-        self._id_token = None
-        self._refresh_token = None
-        self._expiry_date = None
+    def __init__(self, username: str, password: str, *, session: Optional[ClientSession] = None
+    ) -> None:
+        self._username: str = username
+        self._password: str = password
+        self._session: ClientSession = session
+        self._pool_id: str = POOL_ID
+        self._client_id: str = CLIENT_ID
+        self._big_n: int = hex_to_long(N_HEX)
+        self._g: int = hex_to_long(G_HEX)
+        self._k: int = hex_to_long(hex_hash('00' + N_HEX + '0' + G_HEX))
+        self._small_a_value: int = self.generate_random_small_a()
+        self._large_a_value: int = self.calculate_a()
+        self._access_token: Optional[str] = None
+        self._expires_in: Optional[time] = None
+        self._id_token: Optional[str] = None
+        self._refresh_token: Optional[str] = None
+        self._expiry_date: Optional[datetime] = None
 
-    async def auth(self):
+    async def _request(self, method: str, url: str, **kwargs) -> dict:
+        """Make a request against the API."""
+
+        if self._expiry_date and datetime.now() >= self._expiry_date:
+            logging.info("Requesting new access token to replace expired one")
+
+            # Nullify the token so that the authentication request doesn't use it:
+            self._access_token = None
+
+            # Nullify the expiration so the authentication request doesn't get caught
+            # here:
+            self._expiry_date = None
+
+            await self.refreshToken()
+
+        use_running_session = self._session and not self._session.closed
+
+        if use_running_session:
+            session = self._session
+        else:
+            session = ClientSession(timeout=ClientTimeout(total=10))
+
+        try:
+            async with session.request(method, url, **kwargs) as resp:
+                data: dict = await resp.json(content_type=None)
+                resp.raise_for_status()
+                return data
+        except ClientError as err:
+            raise RequestError(f"There was an error while requesting {url}") from err
+        finally:
+            if not use_running_session:
+                await session.close()
+
+    async def async_authenticate(self) -> None:
         """
         Authenticate and store the tokens
         """
@@ -102,97 +138,66 @@ class RinnaiWaterHeater(object):
         "{\"USERNAME\":\"%s\",\"SRP_A\":\"%s\"},\"ClientMetadata\":{}}" 
         % (self._client_id,self._username,format(self._large_a_value,'x')))
 
-        session = ClientSession(timeout=ClientTimeout(total=10))
+        init_auth_response: dict = await self._request(
+            "post",
+            BASE_AUTH_URL,
+            data=payload,
+            headers=INIT_AUTH_HEADERS
+        )
 
-        try:
-            async with session.post(BASE_AUTH_URL,data=payload,headers=INIT_AUTH_HEADERS) as init_auth_resp:
-                init_auth_data: dict = await init_auth_resp.json(content_type = None)
-                init_auth_resp.raise_for_status()
+        logging.info("Successfully sent initAuth")
+        payload = self.process_challenge(init_auth_response['ChallengeParameters'])
 
-                logging.info("Successfully sent initAuth")
-                payload = self.process_challenge(init_auth_data['ChallengeParameters'])
+        respond_auth_response: dict = await self._request(
+            "post",
+            BASE_AUTH_URL,
+            data=payload,
+            headers=RESPOND_TO_AUTH_CHALLENGE_HEADERS
+        )
 
-                try:
-                    async with session.post(BASE_AUTH_URL, data=payload, headers=RESPOND_TO_AUTH_CHALLENGE_HEADERS) as respond_auth_challenge_response:
-                        respond_data: dict = await respond_auth_challenge_response.json(content_type = None)
-                        respond_auth_challenge_response.raise_for_status()
+        logging.info("Successfully responded to Authentication Challenge")
+        self._access_token = respond_auth_response['AuthenticationResult']['AccessToken']
+        self._expires_in = respond_auth_response['AuthenticationResult']['ExpiresIn']
+        self._id_token = respond_auth_response['AuthenticationResult']['IdToken']
+        self._refresh_token = respond_auth_response['AuthenticationResult']['RefreshToken']
+        self._expiry_date = datetime.now() + timedelta(seconds=respond_auth_response['AuthenticationResult']['ExpiresIn'])
 
-                        logging.info("Successfully responded to Authentication Challenge")
-                        self._access_token = respond_data['AuthenticationResult']['AccessToken']
-                        self._expires_in = respond_data['AuthenticationResult']['ExpiresIn']
-                        self._id_token = respond_data['AuthenticationResult']['IdToken']
-                        self._refresh_token = respond_data['AuthenticationResult']['RefreshToken']
-                        self._expiry_date = time.time() + respond_data['AuthenticationResult']['ExpiresIn']
-                except ClientError as err:
-                    raise RequestError(f"There was an error in respond_auth while requesting {BASE_AUTH_URL}") from err
-        except ClientError as err:
-            raise RequestError(f"There was an error in init_auth while requesting {BASE_AUTH_URL}") from err
-        finally:
-            await session.close()
-        return True
-
-    async def refreshToken(self, accessToken):
+    async def refreshToken(self):
         payload = ("{\"ClientId\":\"%s\",\"AuthFlow\":\"REFRESH_TOKEN_AUTH\",\"AuthParameters\":"
-                   "\"REFRESH_TOKEN\":\"%s\"}}" % (self._client_id, accessToken['refresh_token']))
+                    "\"REFRESH_TOKEN\":\"%s\"}}" % (self._client_id, self._refresh_token))
 
-        session = ClientSession(timeout=ClientTimeout(total=10))
+        refresh_response: dict = await self._request(
+            "post",
+            BASE_AUTH_URL,
+            data=payload,
+            headers=INIT_AUTH_HEADERS
+        )
 
-        try:
-            async with session.post(BASE_AUTH_URL, data=payload, headers=INIT_AUTH_HEADERS) as refresh_response:
-                refresh_data: dict = await refresh_refresh_response.json(content_type=None)
-                refresh_response.raise_for_status()
-
-                self._access_token = refresh_data['AuthenticationResult']['AccessToken']
-                self._expires_in = refresh_data['AuthenticationResult']['ExpiresIn']
-                self._id_token = refresh_data['AuthenticationResult']['IdToken']
-                self._refresh_token = refresh_data['AuthenticationResult']['RefreshToken']
-                self._expiry_date = time.time() + refresh_data['AuthenticationResult']['ExpiresIn']
-        except ClientError as err:
-            raise RequestError(f"There was an error refreshing the tokens {BASE_AUTH_URL}") from err
-        finally:
-            await session.close()
-        
-
-    async def __acquireToken(self):
-        # Fetch and refresh tokens as needed
-        data = dict()
-        data["refresh_token"] = self._refresh_token
-
-        self._refresh_token = data['refresh_token']
-        if self._expiry_date:
-            if time.time() >= self._expiry_date:
-                logging.info('No token, or has expired, requesting new token')
-                await self.refreshToken(data)
-        if self._access_token == None:
-            #No existing token exists so refreshing library
-            await self.auth()
-        else:
-            logging.info('Token is valid, continuing')
-            pass
+        self._access_token = refresh_response['AuthenticationResult']['AccessToken']
+        self._expires_in = refresh_response['AuthenticationResult']['ExpiresIn']
+        self._id_token = refresh_response['AuthenticationResult']['IdToken']
+        self._refresh_token = refresh_response['AuthenticationResult']['RefreshToken']
+        self._expiry_date = datetime.now() + timedelta(seconds=refresh_response['AuthenticationResult']['ExpiresIn'])
 
     async def getDevices(self):
-        await self.__acquireToken()
 
         payload = GET_DEVICES_PAYLOAD % (self._username)
-        headers = {
-          'x-amz-user-agent': 'aws-amplify/3.4.3 react-native',
-          'x-api-key': 'da2-dm2g4rqvjbaoxcpo4eccs3k5he',
-          'Content-Type': 'application/json'
+        device_headers = {
+            'x-amz-user-agent': 'aws-amplify/3.4.3 react-native',
+            'x-api-key': 'da2-dm2g4rqvjbaoxcpo4eccs3k5he',
+            'Content-Type': 'application/json'
         }
 
-        session = ClientSession(timeout=ClientTimeout(total=10))
+        data: dict = await self._request(
+            "post",
+            "https://s34ox7kri5dsvdr43bfgp6qh6i.appsync-api.us-east-1.amazonaws.com/graphql",
+            data=payload,
+            headers=device_headers
+        )
 
-        try:
-            async with session.post('https://s34ox7kri5dsvdr43bfgp6qh6i.appsync-api.us-east-1.amazonaws.com/graphql',data=payload,headers=headers) as resp:
-                data: dict = await resp.json(content_type=None)
-                resp.raise_for_status()
-                for items in data["data"]['getUserByEmail']['items']:
-                    for k,v in items['devices'].items():
-                        return v
-        except ClientError as err:
-            raise RequestError(f"There was an error getting the device information") from err
-        finally:
-            await session.close()
+        for items in data["data"]['getUserByEmail']['items']:
+            for k,v in items['devices'].items():
+                return v
 
     @property
     def is_connected(self):
@@ -228,7 +233,7 @@ class RinnaiWaterHeater(object):
         secret_block_b64 = challenge_parameters['SECRET_BLOCK']
 
         timestamp = re.sub(r" 0(\d) ", r" \1 ",
-                               datetime.datetime.utcnow().strftime("%a %b %d %H:%M:%S UTC %Y"))
+                               datetime.utcnow().strftime("%a %b %d %H:%M:%S UTC %Y"))
 
         hkdf = self.get_password_authentication_key(user_id_for_srp, self._password, hex_to_long(srp_b_hex), salt_hex)
         secret_block_bytes = base64.standard_b64decode(secret_block_b64)
@@ -281,5 +286,5 @@ async def async_get_wh(
     username: str, password: str, *, session: Optional[ClientSession] = None
 ) -> RinnaiWaterHeater:
     wh = RinnaiWaterHeater(username,password)
-    await wh.auth()
+    await wh.async_authenticate()
     return wh
